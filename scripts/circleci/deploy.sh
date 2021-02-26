52a07ff35ce58fab97458ed61c8edb5e5968e167
Marvin J. Cortés <marvin.cortes@gmail.com>
2021-02-25 23:12:19 -0600

    Add Pantheon deploy commands.

--- scripts/circleci/deploy.sh
+++ scripts/circleci/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 BRANCH=$1
 MESSAGE=$2
-REPO=$PLATFORM_GIT_REPO
+REPO=$PANTHEON_GIT_REPO
 CLONEFOLDER=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1`
 git clone --branch $BRANCH $REPO $CLONEFOLDER || git clone $REPO $CLONEFOLDER
 cd $CLONEFOLDER
@@ -9,9 +9,17 @@ git branch $BRANCH ; git checkout $BRANCH
 shopt -s extglob
 rm -rf ./!(.git|.|..)
 cd ../
-cp -r .platform config drush files modules settings themes web $CLONEFOLDER/
-cp ./{.environment,.platform.app.yaml,composer.json,composer.lock,composer.patches.json,install-redis.sh,package.json,package-lock.json} $CLONEFOLDER/
+composer install --no-dev --ignore-platform-reqs
+if [ -f ./web/sites/default/settings.secret.php ]; then
+  rm ./web/sites/default/settings.secret.php
+fi
+if [ -d ./web/sites/default/files ]; then
+  rm -rf ./web/sites/default/files
+fi
+cp -r config drush modules scripts/composer settings themes vendor web $CLONEFOLDER/
+cp ./{.drush-lock-update,composer.json,composer.lock,composer.patches.json,package.json,package-lock.json,pantheon.yml} $CLONEFOLDER/
 cd $CLONEFOLDER
+composer prepare-for-pantheon
 # Change CUSTOMTHEME by your own theme folder.
 if [ -f ./themes/custom/CUSTOMTHEME/package.json ]; then
   cd ./themes/custom/CUSTOMTHEME
